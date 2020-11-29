5c10be29d305c965395e597e3b4150a27b4bd7e7
Aditya Pratama <pratamaditya7@gmail.com>
2020-11-29 18:50:54 +0800

    🎨 improve format the code

--- auth/auth.js
+++ auth/auth.js
@@ -7,154 +7,104 @@ const { signupValidation, loginValidation } = require("../db/validation");
 
 // Auth Root Router
 router.get("/", (req, res, next) => {
-	res.json({
-		message: "Welcome to Auth 🚀",
-	});
+   res.json({
+      message: "Welcome to Auth 🚀",
+   });
 });
 
 // Auth Singup Router
-router.post("/signup", (req, res, next) => {
-	// Validation
-	let { error, value } = signupValidation(req.body);
-	if (error) {
-		res.statusCode = 400;
-		next(new Error(error.details[0].message));
-	} else {
-		// Check existing username or email
-		users
-			.findOne({
-				$or: [{ username: value.username }, { email: value.email }],
-			})
-			.then((user) => {
-				if (user) {
-					res.statusCode = 400;
-					next(new Error("email or username is taken 😭"));
-				} else {
-					// Hash password
-					bcrypt
-						.hash(value.password, bcrypt.genSaltSync(10))
-						.then((hashedPassword) => {
-							let newUser = {
-								username: value.username,
-								email: value.email,
-								password: hashedPassword,
-							};
+router.post("/signup", async (req, res, next) => {
+   // Validation
+   let { error, value } = signupValidation(req.body);
+   if (error) {
+      res.statusCode = 400;
+      next(new Error(error.details[0].message));
+   } else {
+      const user = await users.findOne({
+         $or: [{ username: value.username }, { email: value.email }],
+      });
 
-							//Insert new user
-							users
-								.insert(newUser)
-								.then((result) => {
-									delete result.password;
-									const expiration =
-										process.env.COOKIE_EXPIRATION;
-
-									// Generate token
-									generateToken(result, (error, token) => {
-										if (error) {
-											res.statusCode = 400;
-											next(new Error(error));
-										} else {
-											res.cookie("token", token, {
-												expires: new Date(
-													Date.now() +
-														Number(expiration)
-												),
-												secure: false,
-												httpOnly: true,
-											}).json({
-												user: result,
-												token: token,
-											});
-										}
-									});
-								})
-								.catch(() => {
-									res.statusCode = 400;
-									next(new Error("Error insert user. ⚠"));
-								});
-						})
-						.catch(() => {
-							res.statusCode = 400;
-							next(new Error("Error hash password. ⚠"));
-						});
-				}
-			})
-			.catch(() => {
-				res.statusCode = 400;
-				next(new Error("Error find existing user. ⚠"));
-			});
-	}
+      if (user) {
+         res.statusCode = 400;
+         next(new Error("email or username is taken 😭"));
+      } else {
+         const hashedPassword = bcrypt.hashSync(
+            value.password,
+            bcrypt.genSaltSync(10)
+         );
+         if (hashedPassword) {
+            const insertUser = await users.insert({
+               username: value.username,
+               email: value.email,
+               password: hashedPassword,
+            });
+            if (insertUser) {
+               delete insertUser.password;
+               const token = await generateToken(insertUser);
+               if (token) {
+                  const expiration = process.env.COOKIE_EXPIRATION;
+                  res.cookie("token", token, {
+                     expires: new Date(Date.now() + Number(expiration)),
+                     secure: false,
+                     httpOnly: true,
+                  }).json({
+                     user: insertUser,
+                     token: token,
+                  });
+               } else {
+                  res.statusCode = 400;
+                  next(new Error("Whoops! Something went wrong 😭"));
+               }
+            } else {
+               res.statusCode = 400;
+               next(new Error("Error insert user. ⚠"));
+            }
+         } else {
+            res.statusCode = 400;
+            next(new Error("Error hash password. ⚠"));
+         }
+      }
+   }
 });
 
 /* LOGIN ROUTER */
-router.post("/login", function (req, res, next) {
-	// Validation
-	const { error, value } = loginValidation(req.body);
-	if (error) {
-		res.statusCode = 400;
-		next(new Error(error.details[0].message));
-	} else {
-		// username existing
-		users
-			.findOne({ username: value.username })
-			.then((user) => {
-				if (user) {
-					// compare password
-					bcrypt.compare(
-						value.password,
-						user.password,
-						(err, result) => {
-							if (err) {
-								res.statusCode = 400;
-								next(
-									new Error("Whoops! Something went wrong 😭")
-								);
-							} else {
-								if (result) {
-									delete user.password;
-									const expiration =
-										process.env.COOKIE_EXPIRATION;
-
-									// Generate token
-									generateToken(user, (error, token) => {
-										if (error) {
-											res.statusCode = 400;
-											next(new Error(error));
-										} else {
-											res.cookie("token", token, {
-												expires: new Date(
-													Date.now() +
-														Number(expiration)
-												),
-												httpOnly: true,
-												secure: false,
-											}).json({
-												user: user,
-												token: token,
-											});
-										}
-									});
-								} else {
-									res.statusCode = 400;
-									next(
-										new Error(
-											"Username & password not match! 🔑"
-										)
-									);
-								}
-							}
-						}
-					);
-				} else {
-					res.statusCode = 401;
-					next(new Error("User does't exist. ⚠"));
-				}
-			})
-			.catch(() => {
-				res.statusCode = 400;
-				next(new Error("Error find existing user. ⚠"));
-			});
-	}
+router.post("/login", async function (req, res, next) {
+   // Validation
+   const { error, value } = loginValidation(req.body);
+   if (error) {
+      res.statusCode = 400;
+      next(new Error(error.details[0].message));
+   } else {
+      const user = await users.findOne({ username: value.username });
+      if (user) {
+         const match = await bcrypt.compare(value.password, user.password);
+         if (match) {
+            delete user.password;
+            delete user._id;
+            const token = await generateToken(user);
+            if (token) {
+               const expiration = process.env.COOKIE_EXPIRATION;
+               res.cookie("token", token, {
+                  expires: new Date(Date.now() + Number(expiration)),
+                  httpOnly: true,
+                  secure: false,
+               }).json({
+                  user: user,
+                  token: token,
+               });
+            } else {
+               res.statusCode = 400;
+               next(new Error("Whoops! Something went wrong 😭"));
+            }
+         } else {
+            res.statusCode = 400;
+            next(new Error("Username & password not match! 🔑"));
+         }
+      } else {
+         res.statusCode = 401;
+         next(new Error("User does't exist. ⚠"));
+      }
+   }
 });
 
 module.exports = router;
--- auth/generateToken.js
+++ auth/generateToken.js
@@ -1,21 +1,22 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = (data, callback) => {
-	jwt.sign(
-		data,
-		process.env.JWT_SECRET,
-		{
-			expiresIn: process.env.JWT_EXPIRATION,
-			algorithm: process.env.JWT_ALGORITHM,
-		},
-		(error, token) => {
-			if (error) {
-				callback(error, null);
-			} else {
-				callback(null, token);
-			}
-		}
-	);
+const generateToken = (data) => {
+   return new Promise((resolve, reject) => {
+      jwt.sign(
+         data,
+         process.env.JWT_SECRET,
+         {
+            expiresIn: process.env.JWT_EXPIRATION,
+            algorithm: process.env.JWT_ALGORITHM,
+         },
+         (error, token) => {
+            if (error) {
+               reject("Token failed to be generated");
+            } else {
+               resolve(token);
+            }
+         }
+      );
+   });
 };
-
 module.exports = generateToken;
